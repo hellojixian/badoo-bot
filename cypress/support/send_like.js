e92a564b60dcd3a4c18e3a7ff9e3087124abe121
Jixian Wang <jixian@Wangs-MacBook-Pro.local>
2021-01-23 15:45:54 +0100

    update

---
+++ cypress/support/send_like.js
@@ -0,0 +1,31 @@
+Cypress.Commands.add("sendLike", () => {
+  cy.get('html')
+    .trigger('keyup', {keyCode:32, key:' '})
+    .wait(1000)
+    .trigger('keyup', {keyCode:32, key:' '})
+    .wait(1000)
+    .trigger('keyup', {keyCode:49, key:'1'})
+    .wait(1000)
+    .get('html').then(body => {
+      // handle the push notification popup
+      if(body.find('.js-chrome-pushes-allow').length) {
+        cy.get('.js-chrome-pushes-allow').click()
+      }
+      // handle another device login popup
+      if(body.find('.js-continue').length) {
+        cy.get('.js-continue').click()
+      }
+      // handle matches
+      if(body.find('.js-ovl-content .text-field input').length) {
+        cy.get('.js-ovl-content .text-field input')
+          .type('Hi! So happy to meet you!').wait(1000)
+          .get('.js-ovl-content .js-send-message').click()
+      }
+      // handle no more matches
+      if(body.find('.js-import-contacts-items').length) {
+        no_more_match = true
+        return no_more_match
+      }
+      return false
+    })
+})
